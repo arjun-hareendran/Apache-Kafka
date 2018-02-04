a596d5d0f8cc1e248c335913fedab097e08313b1
Arjun Hareendran <arjun.hareeendran@gmail.com>
2018-02-04 11:35:04 +0530

    Producer Code commit

--- src/main/java/com/kafka/v211/producer/KafkaProducer.java
+++ src/main/java/com/kafka/v211/producer/KafkaProducer.java
@@ -1,5 +1,6 @@
 package com.kafka.v211.producer;
 
+import java.sql.Timestamp;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.Producer;
@@ -10,28 +11,27 @@ public class KafkaProducer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Properties prop  = new Properties();
-		
-		//Refer Apache Kafka documentation for the properties used.
-		
-		prop.setProperty("bootstrap.servers", "localhost:9091");
-		prop.setProperty("key.serializer",StringSerializer.class.getName());
-		prop.setProperty("value.serializer",StringSerializer.class.getName());
+
+		Properties prop = new Properties();
+
+		// Refer Apache Kafka documentation for the properties used.
+
+		prop.setProperty("bootstrap.servers", "localhost:9092");
+		prop.setProperty("key.serializer", StringSerializer.class.getName());
+		prop.setProperty("value.serializer", StringSerializer.class.getName());
 		prop.setProperty("acks", "1");
 		prop.setProperty("retries", "2");
 		prop.setProperty("linger.ms ", "1");
-				
-		
-		Producer<String,String> producer  = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(prop);
-		ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("testtopic", "3", "Test Message");
-		producer.send(producerRecord);		
 
-		//linger.ms will overrider
-		//producer.flush();
-		
+		Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(prop);
+		ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("testtopic", "3",
+				"Test Message from java program " + new Timestamp(System.currentTimeMillis()));
+		producer.send(producerRecord);
+	
+		// linger.ms will overrider
+		// producer.flush();
+
 		producer.close();
-		
 		System.out.println("End of Program");
 	}
 
